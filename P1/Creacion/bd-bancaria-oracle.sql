bd5b15223d5a88a59b26d7022cc583ef0ce9cc68
Naxsel <asolanas93@gmail.com>
2016-03-07 17:21:15 +0100

    proyect added

---
+++ P1/Creacion/bd-bancaria-oracle.sql
@@ -0,0 +1,71 @@
+DROP TABLE Pertenece_p1;
+DROP TABLE Oficina_p1;
+DROP TABLE CAhorro_p1;
+DROP TABLE Operacion_p1;
+DROP TABLE Tiene_p1;
+DROP TABLE Cuenta_p1;
+DROP TABLE Usuario_p1;
+
+CREATE TABLE Usuario_p1 (
+  dni VARCHAR(10) PRIMARY KEY,
+  nombre VARCHAR(30) NOT NULL,
+  apellidos VARCHAR(80) NOT NULL,
+  edad NUMBER (3) NOT NULL,
+  direccion VARCHAR(80) NOT NULL,
+  email VARCHAR(80),
+  telefono NUMBER(15)
+);
+
+CREATE TABLE Cuenta_p1 (
+  iban VARCHAR(34) PRIMARY KEY,
+  numero VARCHAR(34) NOT NULL,
+  creacion DATE NOT NULL,
+  saldo FLOAT DEFAULT 0
+);
+
+CREATE TABLE Tiene_p1 (
+  dni VARCHAR(10),
+  iban VARCHAR(34),
+  titular CHAR NOT NULL,
+  FOREIGN KEY (dni) REFERENCES Usuario(dni),
+  FOREIGN KEY (iban) REFERENCES Cuenta(iban),
+  PRIMARY KEY (dni,iban)
+);
+
+CREATE TABLE Operacion_p1 (
+  iban VARCHAR(34),
+  contador INT NOT NULL ,
+  tipo CHAR NOT NULL,
+  fechahora TIMESTAMP NOT NULL,
+  corigen VARCHAR(34),
+  cdestino VARCHAR(34),
+  sucursal VARCHAR(80),
+  concepto VARCHAR(200),
+  FOREIGN KEY (iban) REFERENCES Cuenta(iban),
+  FOREIGN KEY (corigen) REFERENCES Cuenta(iban),
+  FOREIGN KEY (cdestino) REFERENCES Cuenta(iban),
+  PRIMARY KEY (iban,contador)
+);
+
+CREATE TABLE CAhorro_p1 (
+  iban VARCHAR(34) PRIMARY KEY,
+  interes FLOAT NOT NULL,
+  dia NUMBER(2),
+  FOREIGN KEY (iban) REFERENCES Cuenta_p1(iban)
+);
+
+CREATE TABLE Oficina_p1 (
+  codigo NUMBER(4) PRIMARY KEY,
+  direccion VARCHAR(80) NOT NULL ,
+  telefono NUMBER(15) NOT NULL
+);
+
+CREATE TABLE Pertenece_p1 (
+  iban VARCHAR(34),
+  codigo NUMBER(4),
+  FOREIGN KEY (iban) REFERENCES Cuenta(iban),
+  FOREIGN KEY (codigo) REFERENCES Oficina(codigo),
+  PRIMARY KEY (iban,codigo)
+);
+
+SELECT * from DBA_ALL_TABLES;
--- P1/Creacion/tablasTipadasPostgre.sql
+++ P1/Creacion/tablasTipadasPostgre.sql
@@ -1,14 +1,14 @@
-CREATE OR REPLACE TYPE tipoOperacion as ENUM( 'ingresada', 'retirada', 'transferida');
+CREATE TYPE tipoOperacion as ENUM( 'ingresada', 'retirada', 'transferida');
 
-CREATE OR REPLACE TYPE address as OBJECT(
+CREATE TYPE address AS(
 	calle VARCHAR(30),
 	portal VARCHAR(10),
-	piso INT(3),
+	piso INT(3), 
 	ciudad VARCHAR(20),
 	pais VARCHAR(20)
 );
 
-CREATE OR REPLACE TYPE usuario as OBJECT(
+CREATE TYPE usuario AS(
 	dni VARCHAR(9),
 	nombre VARCHAR(20),
 	apellidos VARCHAR(40),
@@ -18,52 +18,50 @@ CREATE OR REPLACE TYPE usuario as OBJECT(
 	telefono INT(9)
 );
 
-CREATE OR REPLACE TYPE cuenta as OBJECT(
+CREATE TYPE cuenta AS(
 	iban VARCHAR(34),
 	numero VARCHAR(34),
 	creacion DATE(40),
-	saldo FLOAT(20)
-)NOT FINAL;
+	saldo FLOAT(20))
+	NOT FINAL;
 
-CREATE OR REPLACE TYPE cuentaCorriente as OBJECT under cuenta();
+CREATE TYPE cuentaCorriente AS(); /*ACABAR*/
 
-CREATE OR REPLACE TYPE cuentaAhorro as OBJECT under cuenta(
+CREATE TYPE cuentaAhorro AS cuenta(
 	interes FLOAT(5),
-	dia DATE
-);
+	dia DATE);
 
-CREATE OR REPLACE TYPE oficina as OBJECT(
+CREATE TYPE oficina AS(
 	codigo VARCHAR(50),
 	direccion address,
 	telefono INT(9)
 );
 
-CREATE OR REPLACE TYPE operacion as OBJECT(
+CREATE TYPE operacion AS(
 	contador INT(50),
-	tipo VARCHAR(20),		--mirar si hacer tipo nuevo
-	fechaHora DATE,		--juntar fecha y hora porque el tipo date en oracle almacena las dos
+	tipo VARCHAR(20),		//mirar si hacer tipo nuevo
+	fechaHora DATE,		//juntar fecha y hora porque el tipo date en oracle almacena las dos
 	cOrigen VARCHAR(34),
 	cDestino VARCHAR(34),
 	sucursal VARCHAR(50),
 	concepto VARCHAR(50)
 );
 
-CREATE TABLE usuarioTabla OF usuario(PRIMARY KEY(dni));
+CREATE TABLE usuarioTabla OF usuario(PRIMARY KEY(dni));		/*añadir constraint?*/
 CREATE TABLE cuentaTabla OF cuenta(PRIMARY KEY(iban));
+CREATE TABLE cuentaCorrienteTabla OF cuentaINHERITS (cuentaTabla);
+CREATE TABLE cuentaAhorroTabla(cuenta cuenta) INHERITS (cuentaTabla);
 CREATE TABLE oficinaTabla OF oficina(PRIMARY KEY(codigo));
 CREATE TABLE operacionTabla OF operacion(PRIMARY KEY(contador));
 
 CREATE TABLE tiene(
 	dni REF(usuarioTabla),
-	iban REF(cuentaTabla)
-);
+	iban REF(cuentaTabla));
 
 CREATE TABLE pertenece(
 	codigo REF(oficinaTabla),
-	iban REF(cuentaTabla)
-);
+	iban REF(cuentaTabla));
 
 CREATE TABLE hace(
 	iban REF(cuentaTabla),
-	contador REF(operacionTabla)
-);
+	contador REF(operacionTabla));
