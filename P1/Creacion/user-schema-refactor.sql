8a73d5fab5673e3af67e6ee613686ab7b9c7f510
naxsel <asolanas93@gmail.com>
2016-03-14 12:41:25 +0100

    new user

--- P1/Creacion/user-schema-refactor.sql
+++ P1/Creacion/user-schema-refactor.sql
@@ -1,23 +1,23 @@
-CREATE TABLESPACE db2_perm_01
-DATAFILE 'db2_perm_01.dat'
+CREATE TABLESPACE bd2_perm_01
+DATAFILE 'bd2_perm_01.dat'
 SIZE 20M
 ONLINE;
 
-CREATE TEMPORARY TABLESPACE db2_temp_01
-TEMPFILE 'db2_temp_01.dbf'
+CREATE TEMPORARY TABLESPACE bd2_temp_01
+TEMPFILE 'bd2_temp_01.dbf'
 SIZE 5M
 AUTOEXTEND ON;
 
-CREATE USER db2
+CREATE USER bd2
 IDENTIFIED BY root
-DEFAULT TABLESPACE db2_perm_01
-TEMPORARY TABLESPACE db2_temp_01
-QUOTA 20M on db2_perm_01;
+DEFAULT TABLESPACE bd2_perm_01
+TEMPORARY TABLESPACE bd2_temp_01
+QUOTA 20M on bd2_perm_01;
 
-GRANT create session TO db2;
-GRANT create table TO db2;
-GRANT create view TO db2;
-GRANT create any trigger TO db2;
-GRANT create any procedure TO db2;
-GRANT create sequence TO db2;
-GRANT create synonym TO db2;
+GRANT create session TO bd2;
+GRANT create table TO bd2;
+GRANT create view TO bd2;
+GRANT create any trigger TO bd2;
+GRANT create any procedure TO bd2;
+GRANT create sequence TO bd2;
+GRANT create synonym TO bd2;
