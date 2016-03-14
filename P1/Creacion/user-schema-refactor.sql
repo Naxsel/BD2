ae25e126f9bd13e5a2e1a032e0ede0a44687a301
naxsel <asolanas93@gmail.com>
2016-03-14 13:30:23 +0100

    privileges

--- P1/Creacion/user-schema-refactor.sql
+++ P1/Creacion/user-schema-refactor.sql
@@ -14,10 +14,4 @@ DEFAULT TABLESPACE bd2_perm_01
 TEMPORARY TABLESPACE bd2_temp_01
 QUOTA 20M on bd2_perm_01;
 
-GRANT create session TO bd2;
-GRANT create table TO bd2;
-GRANT create view TO bd2;
-GRANT create any trigger TO bd2;
-GRANT create any procedure TO bd2;
-GRANT create sequence TO bd2;
-GRANT create synonym TO bd2;
+GRANT ALL PRIVILEGES TO bd2;
