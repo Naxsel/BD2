07b652f797099766200d89f18412c9a94bb5328e
Ana Roig <anagonzalor@gmail.com>
2016-04-01 21:26:29 +0200

    Creado el directorio ed Db4o

---
+++ P1/proyecto-db4o/AccesDB.java
@@ -0,0 +1,12 @@
+import com.sun.org.apache.xml.internal.security.signature.ObjectContainer;
+//import com.db4o.Db4oEmbedded;
+
+/**
+ * Created by anicacortes on 01/04/2016.
+ */
+public class AccesDB {
+
+    public static void main(String[]args) {
+        ObjectContainer bd;
+    }
+}
---
+++ P1/proyecto-db4o/Usuario.java
@@ -0,0 +1,5 @@
+/**
+ * Created by anicacortes on 01/04/2016.
+ */
+public class Usuario {
+}
