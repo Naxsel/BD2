ed633e51709e1e9b18e945cde121195a71009779
ctolon@hotmail.com <ctolon@hotmail.com>
2016-04-18 10:29:54 +0200

    usuario

--- src/entidades/Operacion.java
+++ src/entidades/Operacion.java
@@ -36,4 +36,82 @@ public class Operacion {
 
     @Column(name = "concepto",nullable = false, length = 200)
     private String concepto;
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getContador() {
+        return contador;
+    }
+
+    public void setContador(String contador) {
+        this.contador = contador;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public DATE getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(DATE fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getcDestino() {
+        return cDestino;
+    }
+
+    public void setcDestino(String cDestino) {
+        this.cDestino = cDestino;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    @Override
+    public String toString() {
+        return "Operacion{" +
+                "iban='" + iban + '\'' +
+                ", contador='" + contador + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", fecha=" + fecha +
+                ", hora='" + hora + '\'' +
+                ", cDestino='" + cDestino + '\'' +
+                ", cantidad=" + cantidad +
+                ", concepto='" + concepto + '\'' +
+                '}';
+    }
 }
