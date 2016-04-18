9f3242a0efaecd07bd44d0f61fc66aa95754f3c9
Ana Roig <anagonzalor@gmail.com>
2016-04-18 10:26:59 +0200

    Operacion y OperacionCuenta

---
+++ src/entidades/CuentaOperacion.java
@@ -0,0 +1,14 @@
+package entidades;
+
+import javax.persistence.Column;
+import java.io.Serializable;
+
+/**
+ * Created by anicacortes on 18/04/2016.
+ */
+public class CuentaOperacion implements Serializable {
+
+    private String iban;
+
+    private int contador;
+}
