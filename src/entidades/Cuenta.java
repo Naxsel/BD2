9fdfa5d224073194a620bc294dd83ac287ea38a8
naxsel <asolanas93@gmail.com>
2016-04-18 11:44:28 +0200

    cuentas

--- src/entidades/Cuenta.java
+++ src/entidades/Cuenta.java
@@ -9,15 +9,9 @@ import java.util.Set;
 @Entity(name = "CUENTA")
 public class Cuenta {
 
-    @ManyToMany(mappedBy = "arrayCuentas")
-    private Set<Cliente> arrayClientes;
-
-    @OneToMany(mappedBy = "iban")
-    private Set<Operacion> arrayOperaciones;
-
     @Id
     @Column(name = "IBAN", nullable = false, length = 10)
-    private String dni;
+    private String iban;
 
     @Column(name = "numCuenta", nullable = false, length = 30)
     private String numCuenta;
@@ -28,28 +22,18 @@ public class Cuenta {
     @Column(name = "saldo", nullable = false, length = 3)
     private int saldo;
 
-    public Set<Cliente> getArrayClientes() {
-        return arrayClientes;
+    @ManyToMany(mappedBy = "arrayCuentas")
+    private Set<Cliente> arrayClientes;
+
+    @OneToMany(mappedBy = "iban")
+    private Set<Operacion> arrayOperaciones;
+
+    public String getIban() {
+        return iban;
     }
 
-    public void setArrayClientes(Set<Cliente> arrayClientes) {
-        this.arrayClientes = arrayClientes;
-    }
-
-    public Set<Operacion> getArrayOperaciones() {
-        return arrayOperaciones;
-    }
-
-    public void setArrayOperaciones(Set<Operacion> arrayOperaciones) {
-        this.arrayOperaciones = arrayOperaciones;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
+    public void setIban(String iban) {
+        this.iban = iban;
     }
 
     public String getNumCuenta() {
@@ -76,15 +60,31 @@ public class Cuenta {
         this.saldo = saldo;
     }
 
+    public Set<Cliente> getArrayClientes() {
+        return arrayClientes;
+    }
+
+    public void setArrayClientes(Set<Cliente> arrayClientes) {
+        this.arrayClientes = arrayClientes;
+    }
+
+    public Set<Operacion> getArrayOperaciones() {
+        return arrayOperaciones;
+    }
+
+    public void setArrayOperaciones(Set<Operacion> arrayOperaciones) {
+        this.arrayOperaciones = arrayOperaciones;
+    }
+
     @Override
     public String toString() {
         return "Cuenta{" +
-                "arrayClientes=" + arrayClientes +
-                ", arrayOperaciones=" + arrayOperaciones +
-                ", dni='" + dni + '\'' +
+                "iban='" + iban + '\'' +
                 ", numCuenta='" + numCuenta + '\'' +
                 ", creacion='" + creacion + '\'' +
                 ", saldo=" + saldo +
+                ", arrayClientes=" + arrayClientes +
+                ", arrayOperaciones=" + arrayOperaciones +
                 '}';
     }
 }
