17b894466d853d65aadf76ab7314b1f05f59fcd9
naxsel <asolanas93@gmail.com>
2016-05-08 03:27:06 +0200

    native y jpql

--- src/test/JPQL.java
+++ src/test/JPQL.java
@@ -1,26 +1,73 @@
-//package test;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.EntityManagerFactory;
-//import javax.persistence.Persistence;
-//import javax.persistence.Query;
-//
-///**
-// * Created by naxsel on 5/05/16.
-// */
-//public class JPQL {
-//
-//    public static void main(String[] args) {
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("EjemploDeUnidadDePersistencia");
-//        EntityManager em = entityManagerFactory.createEntityManager();
-//
-//        Query query = em.createQuery("consulta");
+package test;
+
+import parte2.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by naxsel on 5/05/16.
+ */
+public class JPQL {
+
+    public static EntityManagerFactory entityManagerFactory;
+    public static EntityManager em;
+
+    public static void main(String[] args) {
+        entityManagerFactory = Persistence.createEntityManagerFactory("EjemploDeUnidadDePersistencia");
+        em = entityManagerFactory.createEntityManager();
+
+
+        consulta1("31168350Y");
+        System.out.println();
+        consulta2(1986);
+
+//        Query query = em.createQuery("consulta").setParameter ;
 //        List<tipo> list = query.getResultList();
 //
 //        for (tipo e : list) {
 //            System.out.println(e);
 //        }
-//    }
-//
-//
-//}
+    }
+
+    /**
+     * Para un dni devuelve la cuenta asociada (si existe) que más numero de operaciones tiene
+     * @param dni
+     */
+    private static void consulta1(String dni) {
+        System.out.println("Para un dni devuelve la cuenta asociada (si existe) que más numero de operaciones tiene");
+        Query q = em.createQuery("SELECT C.iban,C, O.id.contador FROM Cuenta C join C.arrayOperaciones O join C.arrayUsuarios U WHERE " +
+                "U.dni =:dni AND O.id.contador = (SELECT MAX (id.contador) FROM Operacion Q WHERE Q.iban = C.iban)")
+                .setParameter("dni",dni);
+        List<Object[]> cuentas = q.getResultList();
+        System.out.println("CUENTA,TIPO,CONTADOR");
+        for (Object [] c: cuentas) {
+            for(Object one: c){
+                System.out.print(one + ",");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Devuelve usarios que pertenecen a la oficina indicada
+     * @param oficina
+     */
+    private static void consulta2(int oficina) {
+        System.out.println("Devuelve usarios que pertenecen a la oficina indicada");
+//        Query q = em.createQuery("SELECT U FROM Usuario U join U.arrayCuentas C join C.arrayOperaciones B " +
+//                "join B.oficinaOperacion O WHERE O.codigo = :oficina").setParameter("oficina",oficina);
+        Query q = em.createQuery("SELECT U FROM Usuario U join U.arrayCuentas C WHERE EXISTS " +
+                "(SELECT B FROM CCorriente B join B.oficinaCuenta O WHERE B.iban = C.iban AND O.codigo= :oficina)")
+                .setParameter("oficina",oficina);
+        List<Usuario> usuarios = q.getResultList();
+        for (Usuario c: usuarios) {
+            System.out.println(c);
+        }
+    }
+
+
+}
--- src/test/nativeSQL.java
+++ src/test/nativeSQL.java
@@ -1,7 +1,5 @@
 package test;
 
-import parte2.*;
-
 import javax.persistence.*;
 import java.util.List;
 
@@ -17,16 +15,11 @@ public class nativeSQL {
         entityManagerFactory = Persistence.createEntityManagerFactory("EjemploDeUnidadDePersistencia");
         em = entityManagerFactory.createEntityManager();
         EntityTransaction trans = em.getTransaction();
-        //em.
+
         trans.begin();
         consulta1("31168350Y");
-
-        Query q = em.createNativeQuery("Consulta");
-        List<Object[]> cosa = q.getResultList();
-
-        for (Object[] a : cosa) {
-            System.out.println();
-        }
+        System.out.println();
+        consulta2(1986);
     }
 
     /**
@@ -34,13 +27,39 @@ public class nativeSQL {
      * @param dni
      */
     private static void consulta1(String dni) {
-        Query q = em.createNativeQuery("SELECT * FROM Cuenta_P1 C INNER JOIN Tiene_P1 T ON C.iban = T.iban ");
-        List<Cuenta> cuentas = q.getResultList();
-
-        for (Cuenta c: cuentas) {
-            c.toString();
+        System.out.println("Para un dni devuelve la cuenta asociada (si existe) que más numero de operaciones tiene");
+        Query q = em.createNativeQuery("SELECT C.*, O.contador FROM Cuenta_P1 C INNER JOIN Operacion_P1 O ON C.iban = O.iban " +
+                "WHERE EXISTS (SELECT * FROM Tiene_P1 T WHERE T.iban=C.iban and T.dni='"+dni+"') AND " +
+                "O.contador = (SELECT MAX(contador) FROM Operacion_P1 Q WHERE Q.iban = C.iban)");
+        List<Object[]> cuentas = q.getResultList();
+        System.out.println("IBAN,CREACION,Nº CUENTA, SALDO, CONTADOR");
+        for (Object [] c: cuentas) {
+            for(Object one: c){
+                System.out.print(one + ",");
+            }
+            System.out.println();
         }
-
     }
+
+    /**
+     * Devuelve usarios que pertenecen a la oficina indicada
+     * @param oficina
+     */
+    private static void consulta2(int oficina) {
+        System.out.println("Devuelve usarios que pertenecen a la oficina indicada");
+        Query q = em.createNativeQuery("SELECT U.nombre,U.apellidos,U.dni FROM Usuario_P1 U WHERE EXISTS " +
+                "(SELECT * FROM Tiene_P1 T LEFT JOIN CCorriente_P1 C ON C.iban=T.iban " +
+                "WHERE EXISTS (SELECT * FROM Pertenece_P1 P WHERE P.iban = C.iban AND P.codigo="+oficina+"))");
+        List<Object[]> cuentas = q.getResultList();
+        System.out.println("nombre,apellidos,dni");
+        for (Object [] c: cuentas) {
+            for(Object one: c){
+                System.out.print(one + ",");
+            }
+            System.out.println();
+        }
+    }
+
+
 
 }
